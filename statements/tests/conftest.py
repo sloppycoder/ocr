ccac18c62cacfd4b11abedd7f597ecd46844d456
Li Lin <guru.lin@gmail.com>
2024-04-16 11:11:42 +0800

    added test case for file upload

--- statements/tests/conftest.py
+++ statements/tests/conftest.py
@@ -4,7 +4,7 @@ import sys
 import pytest
 from django.core.management import call_command
 
-from statements.models import Statement
+from statements.models import ApiResponse, Statement
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath(f"{cwd}/.."))
@@ -22,9 +22,20 @@ def django_db_setup(django_db_setup, django_db_blocker):
 
 
 def seed_data():
-    Statement.objects.bulk_create(
-        [
-            Statement(name="doument_1.pdf", mime_type="application.pdf"),
-            Statement(name="statement_2.pdf", mime_type="application.pdf"),
-        ]
-    )
+    test_data_dir = os.path.join(os.path.dirname(__file__), "data")
+
+    with open(f"{test_data_dir}/test_api_response.bin", "rb") as invoice_f:
+        with open(f"{test_data_dir}/test_invoice.bin", "rb") as response_f:
+            response = ApiResponse.objects.create(
+                mime_type="application/pdf",
+                content_sha="58b4ab33560a9f953b8424ad228e0f5016e1ab96",
+                response=invoice_f.read(),
+            )
+
+            Statement.objects.create(
+                name="test_invoice.pdf",
+                mime_type="application/pdf",
+                content_sha="58b4ab33560a9f953b8424ad228e0f5016e1ab96",
+                content=response_f.read(),
+                api_response=response,
+            )
--- statements/tests/test_views.py
+++ statements/tests/test_views.py
@@ -1,13 +1,38 @@
+import os
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from statements.models import Statement
 from statements.urls import app_name
+
+test_data_dir = os.path.join(os.path.dirname(__file__), "data")
 
 
 def test_list_view(client, db):
     response = client.get(f"/{app_name}", follow=True)
     assert response.status_code == 200
     assert bytes("List of Statements", "utf-8") in response.content
+    assert bytes("test_invoice", "utf-8") in response.content
 
 
-def test_upload_view(client, db):
+def test_statement_upload_view(client, db):
     response = client.get(f"/{app_name}/upload", follow=True)
     assert response.status_code == 200
     assert bytes("Upload", "utf-8") in response.content
+
+
+def test_statement_upload(client, db):
+    with open(f"{test_data_dir}/test_invoice.bin", "rb") as f:
+        response = client.post(
+            f"/{app_name}/upload",
+            {
+                "name": "unit_test.pdf",
+                "uploaded_file": SimpleUploadedFile("unit_test.pdf", f.read()),
+            },
+        )
+        # the upload page redirects to list page upon successful upload
+        assert response.status_code == 302
+
+        new_statement = Statement.objects.filter(name="unit_test.pdf").first()
+        # the uploaded statement matches an existing ApiResponse
+        assert new_statement.content_sha == new_statement.api_response.content_sha
